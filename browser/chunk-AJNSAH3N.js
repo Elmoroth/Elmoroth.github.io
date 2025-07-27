7774bb14af177a7ea37c13f9adddd27d61050eff
Elmoroth <deruttertom@gmail.com>
2025-07-27 09:05:52 +0200

    Auto-generated commit [ci skip]

---
+++ browser/chunk-AJNSAH3N.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-7VQIHAKJ.js";import"./chunk-LJVDOMI4.js";export{a as SpeciesComponent};
---
+++ browser/chunk-ENFS2S6M.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-TT2Y3I2V.js";import"./chunk-LJVDOMI4.js";export{a as FamilyTreeBlockComponent};
