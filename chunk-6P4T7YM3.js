8ced09c8d85f4a6228e63ae3307cfc4d1ef1d55c
Elmoroth <deruttertom@gmail.com>
2025-08-03 11:13:25 +0200

    Auto-generated commit [ci skip]

--- chunk-6P4T7YM3.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-YJRONXPQ.js";import"./chunk-AQJAAL4W.js";export{a as SpeciesComponent};
---
+++ chunk-YNL46435.js
@@ -0,0 +1,1 @@
+import{d as a}from"./chunk-RQU2FYGV.js";import"./chunk-AQJAAL4W.js";export{a as SpeciesComponent};
